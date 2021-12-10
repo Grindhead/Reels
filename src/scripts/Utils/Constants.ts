3c0cb9af4674758c0696c40be5ec2890d5cdfc26
Craig Beswetherick <craig@grindheadgames.com>
2021-12-10 11:41:41 +0000

    adding project constants

---
+++ src/scripts/Utils/Constants.ts
@@ -0,0 +1,17 @@
+export const NUM_UNIQUE_SYMBOLS: number = 8;
+export const NUM_SYMBOLS_TO_SHOW: number = 3;
+export const NUM_REELS: number = 5;
+
+export const GAME_WIDTH: number = 1024;
+export const GAME_HEIGHT: number = 768;
+
+export const SYMBOL_WIDTH: number = 236;
+export const SYMBOL_HEIGHT: number = 226;
+
+export const SPIN_START: number = 1.2;
+export const SPIN_DURATION: number = 1;
+export const SPIN_END: number = 0.2;
+
+export const SPIN_SPEED: number = 30;
+
+export const NUM_REEL_STOP_SOUNDS: number = 5;
